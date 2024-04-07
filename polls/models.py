0881a43c2cdc217ed4cacaecc07e32b48ab3909e
Ton Ha <ton@tdha.co>
2024-04-07 18:34:18 +1000

    Set up admin

--- polls/models.py
+++ polls/models.py
@@ -12,13 +12,12 @@ class Question(models.Model):
         return self.question_text
     
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timezone(days=1)
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # e.g. /polls/
     path("", views.index, name="index"),
+    # e.g. /polls/5
+    path("<int:question_id>/", views.detail, name="detail"),
+    # e.g. /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # .e.g /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
