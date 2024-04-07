2ccf18d7eabdab4ed9019026aae74f431d97f7d1
Ton Ha <ton@tdha.co>
2024-04-07 23:04:22 +1000

    Added vote view

--- polls/urls.py
+++ polls/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     # e.g. /polls/
     path("", views.index, name="index"),
     # e.g. /polls/5
-    path("<int:question_id>/", views.detail, name="detail"),
+    path("specifics/<int:question_id>/", views.detail, name="detail"),
     # e.g. /polls/5/results/
     path("<int:question_id>/results/", views.results, name="results"),
     # .e.g /polls/5/vote/
